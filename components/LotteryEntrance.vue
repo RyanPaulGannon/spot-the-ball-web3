7d91d09a916d88872dc981988960356774f45f52
Ryan Gannon <ryanpaulgannon@icloud.com>
2022-12-08 15:03:36 +0000

    Update to Nuxt3, added connectWallet

--- components/LotteryEntrance.vue
+++ components/LotteryEntrance.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
-import { abi, contractAddresses } from "../constants"
-// const lotteryAddress = chainId in contractAddresses ? contractAddresses[chainId][0] : null
-let enterLottery: any
+function enterLottery() {
+  let account
+  ;(window as any).ethereum
+    .request({ method: "eth_requestAccounts" })
+    .then((accounts: any) => {
+      account = accounts[0]
+      console.log(account)
+    })
+}
 </script>
 
 <template>
   <div>Welcome to the lottery!</div>
-  <button @click="enterLottery">Enter Lottery</button>
+  <button id="connectButton" @click="enterLottery">Enter Lottery</button>
 </template>
