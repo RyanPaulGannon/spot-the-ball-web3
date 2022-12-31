5f303f3082696f870e846466b93ca0c910570f76
Ryan Gannon <ryanpaulgannon@icloud.com>
2022-12-31 14:44:40 +0000

    Displays walletAddress after connection

--- components/LotteryEntrance.vue
+++ components/LotteryEntrance.vue
@@ -10,21 +10,31 @@ import { abi, contractAddresses } from "~/constants"
 // const recentWinner = ref("")
 // const entranceFee = ref("")
 // const numOfPlayers = ref("")
+let walletAddress = ref("")
 
-function enterLottery() {
-  let account
-  ;(window as any).ethereum
-    .request({ method: "eth_requestAccounts" })
-    .then((accounts: any) => {
-      account = accounts[0]
-    })
+async function connectWallet() {
+  const provider = new ethers.providers.Web3Provider((window as any).ethereum)
+  await provider.send("eth_requestAccounts", [])
+  const signer = provider.getSigner()
+  walletAddress = await signer.getAddress()
 }
+
+function enterLottery() {}
 </script>
 
 <template>
   <div class="bg-black h-screen">
     <h2 class="text-white text-center pt-2">Welcome to the lottery!</h2>
     <div class="text-center p-4">
+      <button
+        class="rounded bg-white w-5/12 md:w-2/12 lg:w-1/12"
+        @click="connectWallet"
+      >
+        Connect Wallet
+      </button>
+      <br />
+      <p v-if="walletAddress" class="text-white">{{ walletAddress }}</p>
+      <br />
       <button
         class="rounded bg-white w-5/12 md:w-2/12 lg:w-1/12"
         @click="enterLottery"
