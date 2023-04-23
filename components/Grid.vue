cede4196fbb476867af5a118d37ea4f662bb27c3
Ryan Gannon <ryanpaulgannon@icloud.com>
2023-04-23 21:10:37 +0100

    Add grid and stock picture

--- components/Grid.vue
+++ components/Grid.vue
@@ -1,85 +1,78 @@
 <template>
-  <div class="grid">
-    <table class="border-1px border-red-500">
-      <tr class="border-1px border-red-500">
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
+  <table>
+    <tbody>
+      <tr v-for="row in rows" :key="row.id">
+        <td v-for="col in cols" :key="col.id">{{ row.cells[col.id] }}</td>
       </tr>
-      <tr>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-      </tr>
-      <tr>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-      </tr>
-      <tr>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-      </tr>
-      <tr>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-      </tr>
-      <tr>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-      </tr>
-      <tr>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-        <td>...</td>
-      </tr>
-    </table>
-  </div>
+    </tbody>
+  </table>
 </template>
 
+<script setup lang="ts">
+const rows = [
+  {
+    id: 1,
+    cells: ['', '', '', '', '', '', '', ''],
+  },
+  {
+    id: 2,
+    cells: ['', '', '', '', '', '', '', ''],
+  },
+  {
+    id: 3,
+    cells: ['', '', '', '', '', '', '', ''],
+  },
+  {
+    id: 4,
+    cells: ['', '', '', '', '', '', '', ''],
+  },
+  {
+    id: 5,
+    cells: ['', '', '', '', '', '', '', ''],
+  },
+  {
+    id: 6,
+    cells: ['', '', '', '', '', '', '', ''],
+  },
+  {
+    id: 7,
+    cells: ['', '', '', '', '', '', '', ''],
+  },
+]
+const cols = [
+  { id: 0 },
+  { id: 1 },
+  { id: 2 },
+  { id: 3 },
+  { id: 4 },
+  { id: 5 },
+  { id: 6 },
+  { id: 7 },
+]
+</script>
+
 <style>
-.grid {
-  width: 100px;
-}
-.table {
+table {
+  background-image: url('../public/stb.png');
+  height: 400px;
   width: 50%;
+}
+table td {
+  border: 1.5px solid blue;
+}
+td:hover {
+  background-color: rgba(255, 255, 255, 0.5);
+}
+
+@media (max-width: 640px) {
+  table {
+    width: 100%;
+  }
+}
+
+@media (min-width: 641px) {
+  table {
+    width: 50%;
+  }
 }
 </style>
