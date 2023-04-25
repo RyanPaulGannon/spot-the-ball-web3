ed830edf69cfa4e9c80e95eeb1658459dec95703
Ryan Gannon <ryanpaulgannon@icloud.com>
2023-04-25 21:38:35 +0100

    Update dir and config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@pinia/nuxt', '@unocss/nuxt'],
+  ssr: false,
+  modules: ['@pinia/nuxt', '@unocss/nuxt', '@pinia-plugin-persistedstate/nuxt'],
 })
